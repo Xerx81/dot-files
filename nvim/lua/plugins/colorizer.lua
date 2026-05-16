47296b34fc5605c7a9b30eaba48b8d5299e9d06e
Xerx81 <ryuvraj832@gmail.com>
2026-05-16 17:56:17 +0530

    updates

--- nvim/lua/plugins/colorizer.lua
+++ nvim/lua/plugins/colorizer.lua
@@ -1,10 +1,9 @@
 return {
-    "norcalli/nvim-colorizer.lua",
-    config = function()
-        require'colorizer'.setup({
-            '*'; -- Highlight colors in all filetypes
-            css = { rgb_fn = true; }; -- Enable rgb()/rgba() parsing
-            html = { names = true; }; -- Enable color names (like red, blue)
-        })
-    end
+    "catgoose/nvim-colorizer.lua",
+    event = "BufReadPre",
+    opts = {
+        user_default_options = {
+            tailwind = true,
+        },
+    },
 }
