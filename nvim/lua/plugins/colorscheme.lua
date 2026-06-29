cfcc381f1e99199b1c7b4537c0f4efe2bd31cf8c
Xerx81 <ryuvraj832@gmail.com>
2026-06-29 17:22:46 +0530

    added kanagawa colorscheme

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -4,12 +4,13 @@ return {
         -- "ellisonleao/gruvbox.nvim",
         -- "folke/tokyonight.nvim",
         -- "Mofiqul/dracula.nvim",
-        "catppuccin/nvim", name = "catppuccin",
+        -- "catppuccin/nvim", name = "catppuccin",
+        "rebelot/kanagawa.nvim",
         lazy = false, -- make sure we load this during startup if it is your main colorscheme
 		priority = 1000, -- make sure to load this before all the other start plugins
 		config = function()
 			-- load the colorscheme here
-			vim.cmd([[colorscheme catppuccin]])
+			vim.cmd([[colorscheme kanagawa-dragon]])
 
 			vim.api.nvim_set_hl( 0, "Normal", { bg = "none"})
 			vim.api.nvim_set_hl( 0, "NormalFloat", { bg = "none"})
